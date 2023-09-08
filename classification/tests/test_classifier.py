6aa133e6c922d6bbd5fbdbc1f39d750c43ba40ee
rderbier <rderbier@gmail.com>
2023-09-08 09:37:15 -0700

    keep sentence

--- classification/tests/test_classifier.py
+++
@@ -1,6 +0,0 @@
-from src.classifier import Classifier
-
-pipeline = Classifier()
-
-def test_response(requests, response):
-    assert response == pipeline(requests)
--- text-generation/tests/test_text_generator.py
+++
@@ -1,6 +0,0 @@
-from src.text_generator import TextGenerator
-
-pipeline = TextGenerator()
-
-def test_response(requests, response):
-    assert response == pipeline(requests)
